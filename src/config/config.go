291d1458553ba617e974e77d1c62fb6f55f90b45
Edi <edi_vazquez12@hotmail.com>
2022-09-20 23:47:13 -0500

    fix: change load env

--- src/config/config.go
+++ src/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"log"
 	"os"
-	"strings"
 
 	"github.com/Edilberto-Vazquez/website-services/src/constants"
 	"github.com/joho/godotenv"
@@ -14,24 +13,24 @@ type Config struct {
 	Port        string
 }
 
-func getEnvVar(envVar string) (environment string) {
-	switch os.Getenv("GIN_MODE") {
-	case "release":
-		environment = envVar
-	default:
-		environment = strings.Join([]string{"DEV", envVar}, "_")
-	}
-	return
-}
+// func getEnvVar(envVar string) (environment string) {
+// 	switch os.Getenv("GIN_MODE") {
+// 	case "release":
+// 		environment = envVar
+// 	default:
+// 		environment = strings.Join([]string{"DEV", envVar}, "_")
+// 	}
+// 	return
+// }
 
 func NewConfig() (Config, error) {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err)
 	}
-	port := os.Getenv(getEnvVar("PORT"))
-	dbUrl := os.Getenv(getEnvVar("DB_URL"))
-	dbName := os.Getenv(getEnvVar("DB_NAME"))
-	dbCollection := os.Getenv(getEnvVar("DB_COLLECTION"))
+	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DB_URL")
+	dbName := os.Getenv("DB_NAME")
+	dbCollection := os.Getenv("DB_COLLECTION")
 	constants.SetDB(dbName, dbCollection)
 	return Config{
 		Port:        port,
